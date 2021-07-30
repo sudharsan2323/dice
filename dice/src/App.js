9130b74be5fd1aa84191cb18275d74b3e0f2ba9c
sudharsan2323 <87900468+sudharsan2323@users.noreply.github.com>
2021-07-30 19:12:46 +0700

    Add files via upload

---
+++ dice/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import RollDice from './RollDice';
+function App() {
+  return (
+    <div className="App">
+      <RollDice />
+    </div>
+  );
+}
+
+export default App;
